f658340a53fae84e298849ff284c013659aaf8e5
majak96 <majak96@gmail.com>
2018-11-26 18:27:27 +0100

    Male promene u kodu

--- PF1S2.1/World.cs
+++ PF1S2.1/World.cs
@@ -106,25 +106,20 @@ namespace PF1S2._1
             gl.LoadIdentity();
             //gl.PushMatrix();
 
-            gl.Translate(0.0f, 5.0f, -20.0f);
-            gl.Rotate(-1.0f, 0.0f, 0.0f);
+            gl.Translate(0.0f, 5.3f, -20.0f);
+            //gl.Rotate(-1.0f, 0.0f, 0.0f);
 
             gl.Rotate(m_xRotation, 1.0f, 0.0f, 0.0f);
             gl.Rotate(m_yRotation, 0.0f, 1.0f, 0.0f);
 
             drawPodloga(gl);
 
-            gl.Translate(0.0f, 0.01f, 0.0f);
-
             drawStaza(gl);
-
-            gl.Translate(0.0f, 0.01f, 0.0f);
 
             drawLinija(gl);
 
             drawOgrade(gl);
-
-            gl.PushMatrix();
+         
             drawFormula1(gl);
 
             drawFormula2(gl);
@@ -137,70 +132,95 @@ namespace PF1S2._1
         public void drawPodloga(OpenGL gl)
         {
             gl.Begin(OpenGL.GL_QUADS);
-            gl.Color(0.36f, 0.25f, 0.20f);
+            gl.Color(0.36f, 0.25f, 0.20f); //braon podloga
+
             gl.Vertex(-10.0f, -10.0f, 10.0f);
             gl.Vertex(10.0f, -10.0f, 10.0f);
             gl.Vertex(10.0f, -10.0f, -10.0f);
             gl.Vertex(-10.0f, -10.0f, -10.0f);
+
             gl.End();
         }
 
         public void drawStaza(OpenGL gl)
         {
+            gl.Translate(0.0f, 0.01f, 0.0f); //staza je malo iznad podloge
+
             gl.Begin(OpenGL.GL_QUADS);
-            gl.Color(0.32f, 0.32f, 0.32f);
+            gl.Color(0.32f, 0.32f, 0.32f); //siva staza
+
             gl.Vertex(-5.0f, -10.0f, 10.0f);
             gl.Vertex(5.0f, -10.0f, 10.0f);
             gl.Vertex(5.0f, -10.0f, -10.0f);
             gl.Vertex(-5.0f, -10.0f, -10.0f);
+
             gl.End();
         }
 
         public void drawLinija(OpenGL gl)
         {
+            gl.Translate(0.0f, 0.01f, 0.0f); //linija je malo iznad staze
+
             gl.Begin(OpenGL.GL_QUADS);
-            gl.Color(1.0f, 1.0f, 1.0f);
-            gl.Vertex(-0.15f, -10.0f, 10.0f);
-            gl.Vertex(0.15f, -10.0f, 10.0f);
-            gl.Vertex(0.15f, -10.0f, -10.0f);
-            gl.Vertex(0.15f, -10.0f, -10.0f);
+            gl.Color(1.0f, 1.0f, 1.0f); //bela linija
+
+            gl.Vertex(-0.10f, -10.0f, 10.0f);
+            gl.Vertex(0.10f, -10.0f, 10.0f);
+            gl.Vertex(0.10f, -10.0f, -10.0f);
+            gl.Vertex(-0.10f, -10.0f, -10.0f);
+
             gl.End();
         }
 
         public void drawOgrade(OpenGL gl)
         {
+            //leva ograda
             gl.PushMatrix();
-            gl.Color(0.55f, 0.13f, 0.13f);
+            gl.Color(0.55f, 0.13f, 0.13f); //crvena kocka
             gl.Translate(-6f, -9.5f, 0f);
             gl.Scale(0.20f, 0.5f, 10.0f);
+
             Cube cube1 = new Cube();
             cube1.Render(gl, SharpGL.SceneGraph.Core.RenderMode.Render);
+
             gl.PopMatrix();
 
+            //desna ograda
             gl.PushMatrix();
-            gl.Color(0.55f, 0.13f, 0.13f);
+            gl.Color(0.55f, 0.13f, 0.13f); //crvena kocka
             gl.Translate(6f, -9.5f, 0f);
             gl.Scale(0.20f, 0.5f, 10.0f);
+
             Cube cube2 = new Cube();
             cube2.Render(gl, SharpGL.SceneGraph.Core.RenderMode.Render);
+
             gl.PopMatrix();
         }
 
         public void drawFormula1(OpenGL gl)
         {
+            //desna formula
+            gl.PushMatrix();
             gl.Translate(1.2f, -10.1f, 5.0f);
             gl.Scale(0.04f, 0.04f, 0.04f);
             gl.Rotate(0.0f, 180.0f, 0.0f);
+
             m_scene1.Draw();
+
             gl.PopMatrix();
         }
 
         public void drawFormula2(OpenGL gl)
         {
-            gl.Translate(-2.5f, -9.9f, 6.2f);
+            //leva formula
+            gl.PushMatrix();
+            gl.Translate(-2.5f, -9.94f, 6.2f);
             gl.Rotate(90.0f, 180.0f, 0.0f);
             gl.Scale(1.6f, 1.53f, 1.6f);
+
             m_scene2.Draw();
+
+            gl.PopMatrix();
         }
 
         public void drawText(OpenGL gl)
@@ -208,6 +228,7 @@ namespace PF1S2._1
             //2D tekst cyan bojom u donjem desnom uglu
             //font je arial, 14pt, underline
             gl.Viewport(m_width * 2 / 3, 0, m_width / 3, m_height / 3);
+
             gl.DrawText(m_width - 700, 330, 0.0f, 1.0f, 1.0f, "Arial", 14, "Predmet: Racunarska grafika");
             gl.DrawText(m_width - 700, 320, 0.0f, 1.0f, 1.0f, "Arial", 14, "_______________________");
             gl.DrawText(m_width - 700, 260, 0.0f, 1.0f, 1.0f, "Arial", 14, "Sk.god: 2018/19.");
@@ -218,6 +239,7 @@ namespace PF1S2._1
             gl.DrawText(m_width - 700, 110, 0.0f, 1.0f, 1.0f, "Arial", 14, "_______________");
             gl.DrawText(m_width - 700, 50, 0.0f, 1.0f, 1.0f, "Arial", 14, "Sifra zad: 2.1");
             gl.DrawText(m_width - 700, 40, 0.0f, 1.0f, 1.0f, "Arial", 14, "___________");
+
             gl.Viewport(0, 0, m_width, m_height);
         }
 
